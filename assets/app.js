11c7cecda6276bfd772a12262397505acf8acad6
CelineCh49 <celine.chaigne@gmail.com>
2023-10-25 17:39:56 +0200

    ajout de lightbox

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,40 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+// document.querySelectorAll('.image-container img').forEach(image=>{
+// 	image.onclick = ()=>{
+// 		document.querySelector('popup-image').style.display = 'block';
+// 		document.querySelector('popup-image img').src = image.getAttribute('src');
+// 	}
+	
+// });
+
+// document.querySelector('popup-image span').onclick = ()=>{
+// 	document.querySelector('popup-image').style.display = 'none';
+// }
+// Sélectionnez toutes les images dans la classe '.image-container'
+var images = document.querySelectorAll('.image-container img');
+
+// Sélectionnez la boîte modale (popup) et le bouton pour la fermer
+var modal = document.querySelector('.popup-image');
+var closeButton = document.querySelector('.popup-image span');
+
+// Ajoutez un gestionnaire d'événement click à chaque image
+images.forEach(function(image) {
+  image.addEventListener('click', function() {
+    // Affiche la boîte modale
+    modal.style.display = 'block';
+
+    // Définit la source de l'image de la boîte modale sur celle de l'image cliquée
+    var imageSrc = image.getAttribute('src');
+    var modalImage = modal.querySelector('img');
+    modalImage.setAttribute('src', imageSrc);
+  });
+});
+
+// Ajoutez un gestionnaire d'événement click au bouton de fermeture de la boîte modale
+closeButton.addEventListener('click', function() {
+  // Masque la boîte modale
+  modal.style.display = 'none';
+});
